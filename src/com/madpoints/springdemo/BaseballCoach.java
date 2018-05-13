8029488454517ba1160c1ae5cbc0353ff5a03406
John Furtado <jf8846@gmail.com>
2018-05-13 19:17:01 -0400

    added new class

--- src/com/madpoints/springdemo/BaseballCoach.java
+++ src/com/madpoints/springdemo/BaseballCoach.java
@@ -4,6 +4,7 @@ public class BaseballCoach implements Coach {
 
 	@Override
 	public String getDailyWorkout() {
+		
 		return "30 minutes batting practice";
 	}
 	
--- src/com/madpoints/springdemo/MyApp.java
+++ src/com/madpoints/springdemo/MyApp.java
@@ -5,7 +5,9 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		// create object
-		Coach theCoach = new BaseballCoach();
+//		Coach theCoach = new BaseballCoach();
+		
+		Coach theCoach = new TrackCoach();
 		
 		// use object
 		System.out.println(theCoach.getDailyWorkout());
