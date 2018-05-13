c26db346b35a5af8ed6299d1cda440b49313b20e
John Furtado <jf8846@gmail.com>
2018-05-13 19:11:36 -0400

    interface demo

---
+++ src/com/madpoints/springdemo/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.madpoints.springdemo;
+
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "30 minutes batting practice";
+	}
+	
+	
+	
+}
