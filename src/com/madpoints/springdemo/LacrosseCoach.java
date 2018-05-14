95a0f5aae0e5bb6b81171bd7564fd6bfe979cba0
John Furtado <jf8846@gmail.com>
2018-05-13 20:47:46 -0400

    inversion of control with XML configuration

---
+++ src/com/madpoints/springdemo/LacrosseCoach.java
@@ -0,0 +1,10 @@
+package com.madpoints.springdemo;
+
+public class LacrosseCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Run fast break drill for 15 minutes";
+	}
+}
